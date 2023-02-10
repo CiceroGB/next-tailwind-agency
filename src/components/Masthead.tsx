67f7a2b28a6ab8c3ea763cd7678980afe74b0e8f
Cicero Gasparini Brunelli <brunelli.cg@gmail.com>
2023-02-10 12:06:55 -0300

    feat: button scroll down

--- src/components/Masthead.tsx
+++ src/components/Masthead.tsx
@@ -9,28 +9,35 @@ const Masthead: React.FC = () => {
         loop
         muted
         playsInline
-        className="absolute w-full h-full object-cover"
+        className="absolute w-full h-full object-cover "
       >
         <source src="./assets/masthead-bg.m4v" type="video/mp4; codecs=hvc1" />
         <source src="./assets/masthead-bg.mp4" type="video/mp4" />
         <source src="./assets/masthead-bg.webm" type="video/webm; codecs=vp9" />
       </video>
-      <div className="p-12 font-bold z-10 text-white drop-shadow-[0_5px_3px_rgba(0,0,0,0.4)] text-center flex-1 flex items-center justify-center flex-col">
-        <div className="flex-grow-0 pb-20 md:pb-10 transition-all duration-1000">
-          <Image
-            src="./assets/logo.svg"
-            width={128 / 3}
-            height={114 / 3}
-            alt="logo"
-          />
-        </div>
+      <div className="z-10 flex-grow-0 pt-20 md:pt-10 transition-all duration-1000">
+        <Image
+          src="./assets/logo.svg"
+          width={128 / 3}
+          height={114 / 3}
+          alt="logo"
+        />
+      </div>
+      <div className="p-12 font-bold text-white drop-shadow-[0_5px_3px_rgba(0,0,0,0.4)] text-center flex-1 flex items-center justify-center flex-col">
         <h1 className="mb-6 text-4xl xl:text-5xl ">Agency</h1>
         <h2 className="mb-2 text-2xl xl:text-3xl tracking-tight">
           <span>App Development </span>
           <span> done right.</span>
         </h2>
       </div>
-      Masthead
+      <div className="z-10  flex-grow-0 pb-20 md:pb-10 transition-all duration-1000">
+        <Image
+          src="/assets/arrow-down.png"
+          width={188 / 3}
+          height={105 / 3}
+          alt="scroll down"
+        />
+      </div>
     </div>
   );
 };
